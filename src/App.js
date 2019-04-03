00da521f31fdb9cd371a4ce2f3b59675869f2289
Alex Reyes <me@alexjreyes.com>
2019-04-04 01:48:15 +0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="intro">
+            <h1>Hi, I'm <span>Alex</span>. I write <span>code</span> and stuff.</h1>
+          </div>
+
+          <div className="contact">
+            <button>
+              <a href="http://github.com/alexako/" target="_blank">
+                <i className="fa fa-github fa-4x" aria-hidden="true"></i>
+              </a>
+            </button>
+            <button>
+              <a href="http://twitter.com/kludgebot/" target="_blank">
+                <i className="fa fa-twitter fa-4x" aria-hidden="true"></i>
+              </a>
+            </button>
+            <button id="modalBtn" className="copyEmail" data-clipboard-text="me@alexjreyes.com">
+              <i className="fa fa-envelope fa-4x" aria-hidden="true"></i>
+            </button>
+          </div>
+
+          <div id="arrow">
+            <a href="#portfolio">
+              <i className="fa fa-arrow-circle-down fa-4x" aria-hidden="true"></i>
+            </a>
+          </div>
         </header>
       </div>
     );
